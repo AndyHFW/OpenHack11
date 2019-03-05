77dd32d11dff2d94d19ae31d80bc2e6014cd0ed1
Andy Wang <Andy.HF.Wang@gmail.com>
2019-03-05 13:00:27 -0500

    Add python device

---
+++ python-device/Hacker4Device.py
@@ -0,0 +1,67 @@
+# Copyright (c) Microsoft. All rights reserved.
+# Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import random
+import time
+import uuid
+import datetime
+import sys
+
+# Using the Python Device SDK for IoT Hub:
+#   https://github.com/Azure/azure-iot-sdk-python
+# The sample connects to a device-specific MQTT endpoint on your IoT Hub.
+import iothub_client
+# pylint: disable=E0611
+from iothub_client import IoTHubClient, IoTHubClientError, IoTHubTransportProvider, IoTHubClientResult
+from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError, DeviceMethodReturnValue
+
+# The device connection string to authenticate the device with your IoT hub.
+# Using the Azure CLI:
+# az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
+CONNECTION_STRING = "HostName=TorontoOpenHack11.azure-devices.net;DeviceId=Hacker4Device;SharedAccessKey=V6aiBkxaHbp0CNXBIlhLVT39TL718/3gVvyd+gY1kbc="
+
+# Using the MQTT protocol.
+PROTOCOL = IoTHubTransportProvider.MQTT
+MESSAGE_TIMEOUT = 10000
+
+# Define the JSON message to send to IoT Hub.
+TEMPERATURE = 20.0
+HUMIDITY = 60
+MSG_TXT = "{\"ticketId\": %s,\"currentTime\": %s}"
+
+def send_confirmation_callback(message, result, user_context):
+    print ( "IoT Hub responded to message with status: %s" % (result) )
+
+def iothub_client_init():
+    # Create an IoT Hub client
+    client = IoTHubClient(CONNECTION_STRING, PROTOCOL)
+    return client
+
+def iothub_client_telemetry_sample_run():
+
+    try:
+        client = iothub_client_init()
+        print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
+
+        while True:
+            # Build the message with simulated telemetry values.
+            GUID = str(uuid.uuid1())
+            currentTime = str(datetime.datetime.utcnow().isoformat('T') + 'Z')
+            msg_txt_formatted = MSG_TXT % (GUID, currentTime)
+            message = IoTHubMessage(msg_txt_formatted)
+
+            # Send the message.
+            print( "Sending message: %s" % message.get_string() )
+            client.send_event_async(message, send_confirmation_callback, None)
+            time.sleep(1)
+
+    except IoTHubError as iothub_error:
+        print ( "Unexpected error %s from IoTHub" % iothub_error )
+        return
+    except KeyboardInterrupt:
+        print ( "IoTHubClient sample stopped" )
+
+if __name__ == '__main__':
+    print ( "IoT Hub Quickstart #1 - Simulated device" )
+    print ( "Press Ctrl-C to exit" )
+    iothub_client_telemetry_sample_run()
