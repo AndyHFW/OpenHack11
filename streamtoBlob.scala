3238f2e7b04c36951cc77fb0946ef8554025f823
Elena Neroslavskaya <lenisha@gmail.com>
2019-03-06 12:11:22 -0500

    update with final version gson

--- streamtoBlob.scala
+++ streamtoBlob.scala
@@ -20,17 +20,25 @@ dbutils.fs.ls("wasbs://challenge2@messagesiot.blob.core.windows.net/")
 # MAGIC import org.apache.spark.eventhubs._
 # MAGIC import org.apache.spark.sql.types._
 # MAGIC import org.apache.spark.sql.functions._
+# MAGIC import org.apache.spark.sql.streaming.Trigger.ProcessingTime
+# MAGIC 
 # MAGIC 
 # MAGIC val connectionString =  ConnectionStringBuilder("Endpoint=sb://iothub-ns-torontoope-1352814-3aa2009056.servicebus.windows.net/;SharedAccessKeyName=iothubowner;SharedAccessKey=4lT9BzVZzo0U/GW74DTHd2A1cWcrOFNEg9qXp0KNLQ8=;EntityPath=torontoopenhack11").setEventHubName("torontoopenhack11").build
 # MAGIC 
-# MAGIC val customEventhubParameters = EventHubsConf(connectionString)
-# MAGIC   .setMaxEventsPerTrigger(5)
+# MAGIC val customEventhubParameters = EventHubsConf(connectionString).setConsumerGroup("eventhubs")
+# MAGIC   //.setMaxEventsPerTrigger(5)
 # MAGIC val incomingStream = spark.readStream.format("eventhubs").options(customEventhubParameters.toMap).load()
 # MAGIC 
+# MAGIC var streamingSelectDF = 
+# MAGIC   incomingStream
+# MAGIC    .select($"body".cast(StringType), get_json_object(($"body").cast("string"), "$.correlationId").alias("correlationId"),
+# MAGIC            get_json_object(($"body").cast("string"), "$.deviceTime").alias("deviceTime"), date_format(get_json_object(($"body").cast("string"), "$.deviceTime"), "yyyy").alias("year"), date_format(get_json_object(($"body").cast("string"), "$.deviceTime"), "MM").alias("month"), date_format(get_json_object(($"body").cast("string"), "$.deviceTime"), "dd").alias("day"))
 # MAGIC 
-# MAGIC val readInStreamBody = incomingStream.withColumn("body",  $"body".cast(StringType) )
-# MAGIC //readInStreamBody.writeStream.outputMode("append").format("console").option("truncate", false).start().awaitTermination()
-# MAGIC readInStreamBody.writeStream.outputMode("append").format("json").option("path","/mnt/challenge2/messages").option("checkpointLocation","/mnt/challenge2/check").start().awaitTermination()
+# MAGIC streamingSelectDF.writeStream.outputMode("append").format("parquet").partitionBy("year","month","day", "correlationId")
+# MAGIC .option("path",s"/mnt/challenge2/messages")
+# MAGIC .option("checkpointLocation",s"/mnt/challenge2/check")
+# MAGIC .trigger(ProcessingTime("25 seconds"))
+# MAGIC .start().awaitTermination()
 # MAGIC 
 # MAGIC /* val messages =
 # MAGIC       incomingStream
@@ -41,7 +49,6 @@ dbutils.fs.ls("wasbs://challenge2@messagesiot.blob.core.windows.net/")
 # MAGIC       .select("Offset", "Time (readable)", "Timestamp", "Body")
 # MAGIC */
 # MAGIC //messages.printSchema
-# MAGIC 
 # MAGIC 
 # MAGIC // Sending the incoming stream into the console.
 # MAGIC // Data comes in batches!
